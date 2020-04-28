9c80d7532b7703a270ca8802a32e406566cb6617
Samir <sbgray2@illinois.edu>
2020-04-28 16:45:48 -0500

    new UI

--- app/src/main/java/com/example/tarkovbuilder/oldBuildActivity.java
+++ app/src/main/java/com/example/tarkovbuilder/oldBuildActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,14 +16,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class oldBuildActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner1;
+    private Spinner spinner2;
+    private Spinner spinner3;
+    private Spinner spinner4;
+    private Spinner spinner5;
+    private Spinner spinner6;
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_old_build);
         setUpUI();
     }
     private void setUpUI() {
+
         // Save/load buttons
-        Button save = findViewById(R.id.saveButton);
+        Button save = findViewById(R.id.saveBuild);
         save.setOnClickListener(unused -> {
             SaveLoadHandler.save(null);
         });
@@ -30,13 +38,14 @@ public class oldBuildActivity extends AppCompatActivity implements AdapterView.O
         load.setOnClickListener(unused -> {
             SaveLoadHandler.load(null);
         });
+
         // Declare all spinners
-        Spinner spinner1 = findViewById(R.id.spinner);
-        Spinner spinner2 = findViewById(R.id.spinner2);
-        Spinner spinner3 = findViewById(R.id.spinner3);
-        Spinner spinner4 = findViewById(R.id.spinner4);
-        Spinner spinner5 = findViewById(R.id.spinner5);
-        Spinner spinner6 = findViewById(R.id.spinner6);
+        spinner1 = findViewById(R.id.spinner);
+        spinner2 = findViewById(R.id.spinner2);
+        spinner3 = findViewById(R.id.spinner3);
+        spinner4 = findViewById(R.id.spinner4);
+        spinner5 = findViewById(R.id.spinner5);
+        spinner6 = findViewById(R.id.spinner6);
         // Spinner 1 setup
         List<String> spinner1Strings = new ArrayList<>();
         spinner1Strings.add("0");
@@ -45,6 +54,7 @@ public class oldBuildActivity extends AppCompatActivity implements AdapterView.O
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinner1Strings);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(this);
         // Spinner 2 setup
         List<String> spinner2Strings = new ArrayList<>();
         spinner2Strings.add("0");
@@ -52,6 +62,7 @@ public class oldBuildActivity extends AppCompatActivity implements AdapterView.O
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinner2Strings);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
         // Spinner 3 setup
         /* Unused because it is the final entry in that tree
         List<String> spinner3Strings = new ArrayList<>();
@@ -68,6 +79,7 @@ public class oldBuildActivity extends AppCompatActivity implements AdapterView.O
         ArrayAdapter<String> adapter4 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinner4Strings);
         adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner4.setAdapter(adapter4);
+        spinner4.setOnItemSelectedListener(this);
         // Spinner 5 setup
         List<String> spinner5Strings = new ArrayList<>();
         spinner5Strings.add("0");
@@ -75,6 +87,7 @@ public class oldBuildActivity extends AppCompatActivity implements AdapterView.O
         ArrayAdapter<String> adapter5 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinner5Strings);
         adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner5.setAdapter(adapter5);
+        spinner5.setOnItemSelectedListener(this);
         // Spinner 6 needs no setup because it is also a leaf node
 
         /* When 0 is selected, none of the children should display.  When 1 is selected, only one child should display, etc.
@@ -83,6 +96,25 @@ public class oldBuildActivity extends AppCompatActivity implements AdapterView.O
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
+        LinearLayout layout1 = findViewById(R.id.layout_subpart1);
+        LinearLayout layout2 = findViewById(R.id.layout_subpart2);
+        LinearLayout layout3 = findViewById(R.id.layout_subpart3);
+        LinearLayout layout4 = findViewById(R.id.layout_subpart4);
+        LinearLayout layout5 = findViewById(R.id.layout_subpart5);
+        long parentID = parent.getId();
+        if (parentID == R.id.spinner) {
+            if (text.equals("0")) {
+                layout1.setVisibility(LinearLayout.GONE);
+                layout2.setVisibility(LinearLayout.GONE);
+            } else if (text.equals("1")) {
+                layout1.setVisibility(LinearLayout.VISIBLE);
+                layout2.setVisibility(LinearLayout.GONE);
+            } else {
+                layout1.setVisibility(LinearLayout.VISIBLE);
+                layout2.setVisibility(LinearLayout.VISIBLE);
+            }
+       }
+
     }
 
     @Override
